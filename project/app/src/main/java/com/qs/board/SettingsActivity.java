5c259d1c05541189ce3802a9bad0797988e74dcd
enricocid <enrico2588@gmail.com>
2017-12-09 20:34:23 +0100

    Handle permissions
    Signed-off-by: enricocid <enrico2588@gmail.com>

--- project/app/src/main/java/com/qs/board/SettingsActivity.java
+++ project/app/src/main/java/com/qs/board/SettingsActivity.java
@@ -2,8 +2,10 @@ package com.qs.board;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 
@@ -13,10 +15,48 @@ public class SettingsActivity extends Activity {
 
         setContentView(R.layout.settings_activity);
 
+        managePermissions();
+    }
+
+    private void startSettings() {
+
+        Toast.makeText(this, "thankyou:)", Toast.LENGTH_SHORT)
+                .show();
+
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, new SettingsFragment())
                 .commit();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case PermissionUtils.CONTACT_REQUEST_CODE: {
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    PermissionUtils.requestCallPermission(this);
+                }
+            }
+            break;
+
+            case PermissionUtils.CALL_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startSettings();
+                }
+                break;
+        }
+    }
+
+    private void managePermissions() {
+
+        if (PermissionUtils.checkPermission(this, PermissionUtils.READ_CONTACTS) && PermissionUtils.checkPermission(this, PermissionUtils.CALL_PHONE)) {
+
+            PermissionUtils.requestContactPermission(this);
+        }
     }
 
     /**
@@ -26,8 +66,6 @@ public class SettingsActivity extends Activity {
 
         private SharedPreferences.OnSharedPreferenceChangeListener mListenerOptions;
 
-
-        @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
@@ -40,7 +78,7 @@ public class SettingsActivity extends Activity {
                     switch (key) {
 
                         //case SOME_KEY:
-
+                        //do something
                         //break;
                     }
                 }
