1db43e94d0f77cdbbf69cd5defeab3d9e6636701
EnricoD <enrico2588@gmail.com>
2018-01-03 15:10:54 +0100

    Fix broken notification in Android Oreo

--- project/app/src/main/java/com/qs/board/utils/NotificationUtils.java
+++ project/app/src/main/java/com/qs/board/utils/NotificationUtils.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 import com.qs.board.BoardActivity;
@@ -14,11 +15,14 @@ public class NotificationUtils {
 
     private static final String DISMISS_ACTION = "DISMISS";
     private static final int NOT_ID = 1;
+    private static String NOTIFICATION_TITLE;
 
     public static void addNotification(Context context) {
 
         final String CHANNEL_ID = "";
 
+        NOTIFICATION_TITLE = context.getString(R.string.app_name);
+        
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID);
 
         Intent notificationIntent = new Intent(context, BoardActivity.class);
@@ -29,8 +33,8 @@ public class NotificationUtils {
         PendingIntent dismissIntent = PendingIntent.getBroadcast(context, 2588, cancelReceive, PendingIntent.FLAG_UPDATE_CURRENT);
 
         notificationBuilder.setSmallIcon(R.drawable.ic_add_circle)
-                .setTicker(context.getString(R.string.app_name))
-                .setContentTitle(context.getString(R.string.app_name))
+                .setTicker(NOTIFICATION_TITLE)
+                .setContentTitle(NOTIFICATION_TITLE)
                 .setOngoing(true)
                 .addAction(R.drawable.ic_close_black, context.getString(android.R.string.cancel), dismissIntent)
                 .setContentText(context.getString(R.string.notification_content));
@@ -44,6 +48,14 @@ public class NotificationUtils {
         notificationBuilder.setContentIntent(intent);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+        // Create notification channel on Android O
+        NotificationChannel channel = new NotificationChannel(NOTIFICATION_TITLE, NOTIFICATION_TITLE, notificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(channel);
+
+        }
 
         if (notificationManager != null) {
             notificationManager.notify(NOT_ID, notificationBuilder.build());
